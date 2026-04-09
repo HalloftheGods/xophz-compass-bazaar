bef1711d59229ddf6d4c01487ac942d65e0df8d6
Xopher <xopher@vi.co>
2026-04-09 05:48:56 -0700

    chore: update plugin author metadata to Hall of the Gods, Inc. a

--- xophz-compass-bazaar.php
+++ xophz-compass-bazaar.php
@@ -18,7 +18,7 @@
  * Plugin URI:        https://www.mycompassconsulting.com/bazaar/
  * Description:       This supercharged UI helps you quickly manage your woocommerce inventory, orders, and view sales data in real time.
  * Version:           1.0.0
- * Author:            Xoph
+ * Author:            Hall of the Gods, Inc.
  * Author URI:        http://example.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
